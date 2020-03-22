ac0affb99d79ecd62f75a9e137e1e76313d1a6a3
SchmitPSL <schmitpsl@yahoo.com>
2020-03-22 18:54:53 -0400

    Initial

---
+++ Schmit_Problem1/src/DuplicateRemover.java
@@ -0,0 +1,17 @@
+import java.io.FileReader;
+
+public class DuplicateRemover {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+try {
+	BufferedReader reader = new BufferedReader (new FileReader (myfile));
+}catch (IOException e) {
+	
+}
